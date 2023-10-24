cd1058302011f7c2746fa5bf24e8376d6a273768
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-23 19:42:18 -0500

    gt

--- commands/eval.js
+++ commands/eval.js
@@ -28,9 +28,9 @@ class Command {
                 result = execSync(`proot-distro login ubuntu -- bash -c 'node -e "eval\(${command}\)"'`).toString().replaceAll("\\n", "").replaceAll("\n", "");
             } else {
                 result = execSync(`proot-distro login ubuntu -- bash -c 'node -e "${command}"'`).toString().replaceAll("\\n", "").replaceAll("\n", "");
-            }
+            }*/
             console.log(result.length);
-            console.log(command)*/
+            console.log(command)
         } catch (err) {
             result = String(err);
             failed = true;
