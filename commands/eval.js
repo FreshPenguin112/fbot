8a6e45d4954550f9db3d0a7c89bce616f3c89dbd
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-23 20:36:10 -0500

    f

--- commands/eval.js
+++ commands/eval.js
@@ -33,7 +33,7 @@ class Command {
         } catch (err) {
             result = "the j";// + ":" + err.message;
             console.log(Object.getOwnPropertyNames(err));
-            console.log(err.output.toString());
+            console.log(err.stderr.toString());
             //result = "lmao you did a error somewhere nerd :nerdclown: :haha:"
             failed = true;
 
