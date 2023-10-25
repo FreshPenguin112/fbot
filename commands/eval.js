696b558cf98bb83d8ea6014fbb38153a0f220306
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-25 17:40:22 -0500

    yhgttyh

--- commands/eval.js
+++ commands/eval.js
@@ -24,18 +24,18 @@ class Command {
             let b = btoa(command);
             let k = require("randomstring").generate();
             let k2 = require("randomstring").generate();
-            result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js && rm -rf ${k}.js ${k2}.txt'`);
+            result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js'`);
             result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
             console.log(result.length);
             if (result.length === 0){
                 console.log("doing eval instead");
                 command = `eval(\`${command}\`)`;
                 b = btoa(command);
-                result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js && rm -rf ${k}.js ${k2}.txt'`);
+                result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js'`);
                 result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
                 console.log(result);
             }
-            //execSync(`proot-distro login ubuntu --isolated -- eval 'rm ${k}.js && rm ${k2}.txt'`);
+            execSync(`proot-distro login ubuntu --isolated -- eval 'rm -rf ${k}.js ${k2}.txt'`);
             //result = a;
             /*if (result.length === 0) {
                 console.log("changing result");
