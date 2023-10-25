bf92f6ba85a200ac259f0a605fa7bbc20111dc61
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-25 18:15:24 -0500

    frgrrggrgr

--- commands/eval.js
+++ commands/eval.js
@@ -29,14 +29,7 @@ class Command {
             //console.log(result.length);
             if (result.length === 0) {
                 //console.log("doing eval instead");
-                command = `console.log(${command})`;
-                b = btoa(command);
-                result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js && rm -rf ${k}.js ${k2}.txt'`);
-                result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
-                //console.log(result);
-            } else if (result.length === 0) {
-                //console.log("doing eval instead");
-                command = `console.log(eval(\`${command}\`))`;
+                command = `try{console.log(${command})}catch(e){console.log(eval(\`${command}\`))}`;
                 b = btoa(command);
                 result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js && rm -rf ${k}.js ${k2}.txt'`);
                 result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
