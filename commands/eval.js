9e08508efa63f9bfd15e1c0230db787bdf1a42c5
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-25 19:07:31 -0500

    rf

--- commands/eval.js
+++ commands/eval.js
@@ -29,11 +29,11 @@ class Command {
             //console.log(result.length);
             if (result.length === 0) {
                 //console.log("doing eval instead");
-                command = JSON.stringify(require("uglify-js").minify(command, {dead_code: false}));
+                command = require("uglify-js").minify(command, {dead_code: false});
                 console.log(command.error);
                 command = command.code;
                 console.log(command);
-                command = `console.log(eval(${command}))`;
+                command = `console.log(eval(${JSON.stringify(command)}))`;
                 console.log(command);
                 b = btoa(command);
                 result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.js && node ${k}.js && rm -rf ${k}.js ${k2}.txt'`);
