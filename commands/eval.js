a22de5237b06b81d393884be871cc71ae7b89b8a
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-10-31 16:28:28 -0500

    ffff

--- commands/eval.js
+++ commands/eval.js
@@ -48,7 +48,7 @@ class Command {
             result = exec(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.${type} && ${runner} ${k}.${type} && rm -rf ${k}.${type} ${k2}.txt'`);
             var w = a => {result = a}
             result.stdout.on("data", d => {console.log(d+"test");w(d)});
-            for (var i in cargs) result.stdin.write(i + "\n")
+            cargs.forEach(f=>result.stdin.write(f + "\n"))
             result.stdin.end();
             result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
             //console.log(result.length);
