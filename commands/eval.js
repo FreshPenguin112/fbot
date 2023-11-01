0b1aaf9eeb3760539301b418aef3b060282fde20
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-01 17:05:24 -0500

    aaaaaaaaaaaaa

--- commands/eval.js
+++ commands/eval.js
@@ -46,13 +46,13 @@ class Command {
                 runner = "node";
                 type = "js";
             }
-            var y = [];
+            var y = {a:[]}
             const pr = exec(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.${type} && ${runner} ${k}.${type} && rm -rf ${k}.${type} ${k2}.txt'`, (error, stdout, stderr, y) => {
                 if (!!error) {
                     throw error;
                 }
                 console.log(typeof y)
-                y = [stdout.toString()]
+                y.a = [stdout.toString()]
                 console.log(stdout.toString());
                 //global.y += stdout.toString();
             });
