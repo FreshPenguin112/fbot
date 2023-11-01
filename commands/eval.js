014c7a94bb6bbb63590c0c3d9edb66135ad7d039
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-01 14:22:08 -0500

    ddddddddddddddddddddd

--- commands/eval.js
+++ commands/eval.js
@@ -61,7 +61,7 @@ class Command {
                 }
               })
             for (let i of cargs) {st.write(i);}
-            result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.${type} && ${runner} ${k}.${type} && rm -rf ${k}.${type} ${k2}.txt'`, {stdio:st});
+            result = execSync(`proot-distro login ubuntu --isolated -- eval 'echo "${b}" > ${k2}.txt && echo "$(base64 --decode ${k2}.txt)" > ${k}.${type} && ${runner} ${k}.${type} && rm -rf ${k}.${type} ${k2}.txt'`, {stdio:[null, st, process.stderr]});
             result = result.toString().replaceAll("\\n", "").replaceAll("\n", "");
             //console.log(result.length);
             if (!1/*result.length === 0*/) {
