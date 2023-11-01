0fb7e19db0b87cc7c2052f2538cc6e3375ce5c27
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-01 09:15:12 -0500

    aaaaaaaaa

--- commands/eval.js
+++ commands/eval.js
@@ -20,6 +20,8 @@ class Command {
             let py = command.includes("#py")||command.includes("# py");
             let cargsindex = command.split("\n").findIndex(x => x.startsWith("#args")||x.startsWith("# args")||x.startsWith("//args")||x.startsWith("// args"));
             let cargs = command.split("\n")[cargsindex].replace("# args ", "").replace("#args ", "").replace("// args ", "").replace("//args ", "").split(";");
+            cargs = new ArrayBuffer(cargs);
+            cargs = new DataView(cargs);
             console.log(py);
             console.log('\n');
             console.log(`${message.author.username}:`);
